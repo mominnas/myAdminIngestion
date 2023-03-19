6cb7c3992758fe25fe079f12e842301e3b689e3a
Momin Naseem <mominn3935@gmail.com>
2023-03-19 15:42:15 -0400

    Update dependencies and restructure project

--- thibert.py
+++ thibert.py
@@ -2,7 +2,7 @@
 import json
 import requests
 from typing import Dict, List, Union, Tuple, Optional
-from tapi_client.client import Client, AuthenticatedClient
+from tapi_client import Client, AuthenticatedClient
 import cred
 
 
@@ -10,7 +10,7 @@ import cred
 
 TAPI_KEY: str = cred.TAPI_KEY
 JSON_URL: str = cred.JSON_URL
-
+BASE_URL: str = cred.THIBERT_BASE_URL
 
 def get_json() -> Dict[str, Union[str, Dict]]:
     """Get the JSON response from the API.
@@ -28,15 +28,15 @@ def get_json() -> Dict[str, Union[str, Dict]]:
 
 
 def get_api_paths(response_info: Dict[str, Union[str, Dict]]) -> None:
-    """ Return the paths to the API method endpoints.
+    """Return the paths to the API method endpoints.
 
     Args:
-        response_info (Dict[str, Union[str, Dict]]): Dictionary of the OpenAPI json from the endpoint.
+        response_info (Dict[str, Union[str, Dict]]): Dictionary of the 
+            OpenAPI json from the endpoint.
     """
     if response_info.get('paths') is None:
         print("No paths found.")
-        return
-    
+        return None
     for key in response_info['paths']:
         print(key)
 
@@ -51,15 +51,14 @@ def generate_client(key: str=TAPI_KEY) -> Client:
     Returns:
         Client: _description_
     """
-    client: Client = AuthenticatedClient(base_url="https://api.example.com", token=key, 
-                                         timeout=50, verify_ssl=False, raise_on_unexpected_status=True)
+    client: Client = AuthenticatedClient(base_url=BASE_URL, token=key,
+                                         timeout=50, verify_ssl=False,
+                                         raise_on_unexpected_status=True)
 
     return client
 
 
-if __name__ == "__main__":
-    
+if __name__ == "__main__": 
     r_info: Dict[str, Union[str, Dict]] = get_json()
     get_api_paths(r_info)
-    
     api_client = generate_client()
